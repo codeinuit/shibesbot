ab18c19b90159242e9a4ee730fe8a715788ccb9f
Lucas DEBOUTÉ <P147x.x@gmail.com>
2018-04-25 22:23:24 +0200

    bot optimisation

--- shibes.py
+++ shibes.py
@@ -16,15 +16,9 @@ bot_prefix = "shibes"
 client = commands.Bot(command_prefix=bot_prefix)
 token = "NDM4NzcxMzI2Mzg4MjczMTUz.DcJedQ.HnyD8BMdnmIjhaGgWwJEStuHDvQ-Q"
 
-async def on_shibes_ask(message):
+@bot.command()
+async def shibes():
     response = requests.get('http://shibe.online/api/shibes')
-    await client.send_message(message.channel, response.json()[0])
-
-
-@client.event
-async def on_message(message):
-    if message.content.startswith(bot_prefix):
-        await on_shibes_ask(message)
-
+    await client.send_message(message.channel, response.json()[0]
 
 client.run("NDM4NzcxMzI2Mzg4MjczMTUz.DcJfAQ.K9sAt-bd9pQWu49i9MC4Eo7lo1k")
