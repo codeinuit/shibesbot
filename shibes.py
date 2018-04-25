e72d108cfc85d23fd9eac1a44a6772a39b2e5f05
Lucas DEBOUTÉ <P147x.x@gmail.com>
2018-04-25 21:38:54 +0200

    v&

---
+++ shibes.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+
+import json
+import requests
+
+""" 
+"" Discord configuration
+"""
+
+Client = discord.Client()
+bot_prefix = "shibes"
+client = commands.Bot(command_prefix=bot_prefix)
+token = "NDM4NzcxMzI2Mzg4MjczMTUz.DcJedQ.HnyD8BMdnmIjhaGgWwJEStuHDvQ-Q"
+
+async def on_shibes_ask(message):
+    response = requests.get('http://shibe.online/api/shibes')
+    await client.send_message(message.channel, response.json()[0])
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith(bot_prefix):
+        await on_shibes_ask(message)
+
+
+client.run("NDM4NzcxMzI2Mzg4MjczMTUz.DcJfAQ.K9sAt-bd9pQWu49i9MC4Eo7lo1k")
