b27682e00c433079face2e8cf20cfa6fed6631ee
Lucas DEBOUTÉ <P147x.x@gmail.com>
2018-04-25 22:26:35 +0200

    fix

--- shibes.py
+++ shibes.py
@@ -19,6 +19,13 @@ token = "NDM4NzcxMzI2Mzg4MjczMTUz.DcJedQ.HnyD8BMdnmIjhaGgWwJEStuHDvQ-Q"
 @bot.command()
 async def shibes():
     response = requests.get('http://shibe.online/api/shibes')
-    await client.send_message(message.channel, response.json()[0]
+    await client.send_message(message.channel, response.json()[0])
+
+@bot.event
+    async def on_ready():
+        print('Logged in as')
+        print(bot.user.name)
+        print(bot.user.id)
+        print('------')
 
 client.run("NDM4NzcxMzI2Mzg4MjczMTUz.DcJfAQ.K9sAt-bd9pQWu49i9MC4Eo7lo1k")
