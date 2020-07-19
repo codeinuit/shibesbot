f218b3dbace69178551bfabc4bc088cd96a2a8bd
P147x <lucas.deboute@gmail.com>
2020-07-19 19:43:23 +0000

    feat(better-logs): add logrus as logger

--- discord.go
+++ discord.go
@@ -18,7 +18,7 @@ var (
 )
 
 func initDiscord(t string) {
-	dg, err := discordgo.New("Bot " + Token)
+	dg, err := discordgo.New("Bot " + t)
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
 		return
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/bwmarrin/discordgo v0.21.1
 	github.com/ivolo/go-giphy v0.0.0-20150927080050-d31b832022e4
+	github.com/sirupsen/logrus v1.6.0
 )
