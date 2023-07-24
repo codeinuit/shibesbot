f3a79e31ad376cd226b9a89ca1f3946d829624e3
Lucas D (inu) <lucas.deboute@gmail.com>
2023-07-24 16:01:11 +0200

    Create local storage implementation for caching

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -2,10 +2,21 @@ package cache
 
 import "context"
 
-// Cache is an interface for cache implementations
+// Cache is used as the main implementation for caching libraries
 type Cache interface {
+	// Get returns the assigned value for a given key
 	Get(context.Context, string) (any, error)
-	Set(context.Context, string, string) (any, error)
+
+	// Set registers a value, regardless if the key as been
+	// already set or not.
+	Set(context.Context, string, any) (any, error)
+
+	// Incr increments an value for a given key.
+	// If the key does not exists, Incr assumes the default
+	// value is 0 and will return 1.
 	Incr(context.Context, string) (any, error)
-	SetNX(context.Context, string, any) (any, error)
+
+	// SetNX check if the key is already defined, and return
+	// the result as a boolean value.
+	SetNX(context.Context, string, any) (bool, error)
 }
