4823244207f309163fef06207c44edfdbe6cdd8a
inu (Lucas DEBOUTE) <lucas.deboute@gmail.com>
2024-03-06 13:05:55 -0800

    feat(logger) support formatting

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -3,6 +3,9 @@ package logger
 // Logger interface implementation
 type Logger interface {
 	Info(...any)
+	Infof(string, ...any)
 	Warn(...any)
+	Warnf(string, ...any)
 	Error(...any)
+	Errorf(string, ...any)
 }
